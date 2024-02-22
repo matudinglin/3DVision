f3f30b5ce9497e3e8d497b2123e7c903ff267e16
matudinglin <45813154+matudinglin@users.noreply.github.com>
2024-02-21 22:15:26 -0800

    Complete assignment

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# CSCI599
-3D Vision Assignment Template for Spring 2024.
+# Assignment Overview
 
-The following tutorial will go through you with how to use three.js for your assignment visualization. Please make sure your VScode is installed with "Live Server" plugin.
+Here is the checklist for the assignment:
 
-## How to use
-```shell
-git clone https://github.com/jingyangcarl/CSCI599.git
-cd CSCI599
-ls ./ # you should see index.html and README.md showup in the terminal
-code ./ # open this folder via vscode locally
-# open and right click on the index.html
-# select "Open With Live Server" to run the code over localhost.
-```
+- [x] Loop subdivision.
+- [x] Implement Quadratic Error based mesh decimation.
+- [x] Write up your project, details of data structure, algorithms, reporting runtime, and visualizations of results with different parameters.
+- [x] Extra credit: Analyze corner cases (failure cases) and find solutions to prevent them.
+- [x] Extra credit: Implement Loop subdivision using two different data structures.
+- [x] Extra credit: Preserve the original vertices after decimation.
+
+**Note:** I use the OpenMesh library for the halfedge data structure. Please install OpenMesh before running the code. Please note that the OpenMesh library only supports Python version no later than 3.9.
+
+Additional results can be found in the folder: `assets/assignment1`.
+
+Open assignment1.html to see the writeup report.
--- code/assignment1.py
+++ code/assignment1.py
@@ -1,9 +1,139 @@
 import openmesh as om
 import numpy as np
-import heapq
+import trimesh
 import cProfile
+from trimesh import graph, grouping
+from trimesh.geometry import faces_to_edges
+from itertools import zip_longest
 
-def subdivision_loop(mesh, iterations=1):
+def subdivision_loop_array(mesh, iterations=1):
+    """
+    Apply Loop subdivision to the input mesh for the specified number of iterations.
+    :param mesh: input mesh
+    :param iterations: number of iterations
+    :return: mesh after subdivision
+    """
+    for _ in range(iterations):
+        # prepare geometry for the loop subdivision
+        vertices, faces = mesh.vertices, mesh.faces # [N_vertices, 3] [N_faces, 3]
+        edges, edges_face = faces_to_edges(faces, return_index=True) # [N_edges, 2], [N_edges]
+        edges.sort(axis=1)
+        unique, inverse = grouping.unique_rows(edges)
+        
+        # split edges to interior edges and boundary edges
+        edge_inter = np.sort(grouping.group_rows(edges, require_count=2), axis=1)
+        edge_bound = grouping.group_rows(edges, require_count=1)
+        
+        # set also the mask for interior edges and boundary edges
+        edge_bound_mask = np.zeros(len(edges), dtype=bool)
+        edge_bound_mask[edge_bound] = True
+        edge_bound_mask = edge_bound_mask[unique]
+        edge_inter_mask = ~edge_bound_mask
+        
+        ###########
+        # Step 1: #
+        ###########
+        # Calculate odd vertices to the middle of each edge.
+        odd = vertices[edges[unique]].mean(axis=1) # [N_oddvertices, 3]
+        
+        ###########
+        # Step 2: #
+        ###########
+        # find v0, v1, v2, v3 and each odd vertex
+        # v0 and v1 are at the end of the edge where the generated odd vertex on
+        # locate the edge first
+        e = edges[unique[edge_inter_mask]]
+        # locate the endpoints for each edge
+        e_v0 = vertices[e][:, 0]
+        e_v1 = vertices[e][:, 1]
+        
+        # v2 and v3 are at the farmost position of the two triangle
+        # locate the two triangle face
+        edge_pair = np.zeros(len(edges)).astype(int)
+        edge_pair[edge_inter[:, 0]] = edge_inter[:, 1]
+        edge_pair[edge_inter[:, 1]] = edge_inter[:, 0]
+        opposite_face1 = edges_face[unique]
+        opposite_face2 = edges_face[edge_pair[unique]]
+        # locate the corresponding edge
+        e_f0 = faces[opposite_face1[edge_inter_mask]]
+        e_f1 = faces[opposite_face2[edge_inter_mask]]
+        # locate the vertex index and vertex location
+        e_v2_idx = e_f0[~(e_f0[:, :, None] == e[:, None, :]).any(-1)]
+        e_v3_idx = e_f1[~(e_f1[:, :, None] == e[:, None, :]).any(-1)]
+        e_v2 = vertices[e_v2_idx]
+        e_v3 = vertices[e_v3_idx]
+        
+        # update the odd vertices based the v0, v1, v2, v3, based the following:
+        # 3 / 8 * (e_v0 + e_v1) + 1 / 8 * (e_v2 + e_v3)
+        odd[edge_inter_mask] = 0.375 * e_v0 + 0.375 * e_v1 + e_v2 / 8.0 + e_v3 / 8.0
+        
+        ###########
+        # Step 3: #
+        ###########
+        # find vertex neightbors for even vertices and update accordingly
+        neighbors = graph.neighbors(edges=edges[unique], max_index=len(vertices))
+        # convert list type of array into a fixed-shaped numpy array (set -1 to empties)
+        neighbors = np.array(list(zip_longest(*neighbors, fillvalue=-1))).T
+        # if the neighbor has -1 index, its point is (0, 0, 0), so that it is not included in the summation of neighbors when calculating the even
+        vertices_ = np.vstack([vertices, [0.0, 0.0, 0.0]])
+        # number of neighbors
+        k = (neighbors + 1).astype(bool).sum(axis=1)
+        
+        even = np.zeros_like(vertices)
+        
+        # calculate even vertices for the interior case
+        beta = (40.0 - (2.0 * np.cos(2 * np.pi / k) + 3) ** 2) / (64 * k)
+        even = (
+            beta[:, None] * vertices_[neighbors].sum(1)
+            + (1 - k[:, None] * beta[:, None]) * vertices
+        )
+        
+        # calculate even vertices for the boundary case
+        if edge_bound_mask.any():
+            # boundary vertices from boundary edges
+            vrt_bound_mask = np.zeros(len(vertices), dtype=bool)
+            vrt_bound_mask[np.unique(edges[unique][~edge_inter_mask])] = True
+            # boundary neighbors
+            boundary_neighbors = neighbors[vrt_bound_mask]
+            boundary_neighbors[~vrt_bound_mask[neighbors[vrt_bound_mask]]] = -1
+
+            even[vrt_bound_mask] = (
+                vertices_[boundary_neighbors].sum(axis=1) / 8.0
+                + (3.0 / 4.0) * vertices[vrt_bound_mask]
+            )
+        
+        ############
+        # Step 1+: #
+        ############
+        # complete the subdivision by updating the vertex list and face list
+
+        
+        # the new faces with odd vertices
+        odd_idx = inverse.reshape((-1, 3)) + len(vertices)
+        new_faces = np.column_stack(
+            [
+                faces[:, 0],
+                odd_idx[:, 0],
+                odd_idx[:, 2],
+                odd_idx[:, 0],
+                faces[:, 1],
+                odd_idx[:, 1],
+                odd_idx[:, 2],
+                odd_idx[:, 1],
+                faces[:, 2],
+                odd_idx[:, 0],
+                odd_idx[:, 1],
+                odd_idx[:, 2],
+            ]
+        ).reshape((-1, 3)) # [N_face*4, 3]
+
+        # stack the new even vertices and odd vertices
+        new_vertices = np.vstack((even, odd)) # [N_vertex+N_edge, 3]
+        
+        mesh = trimesh.Trimesh(new_vertices, new_faces)
+    return mesh
+
+def subdivision_loop_halfedge(mesh, iterations=1):
     """
     Apply Loop subdivision to the input mesh for the specified number of iterations.
     :param mesh: input mesh
@@ -174,16 +304,22 @@ def simplify_quadric_error(mesh, face_count=1, subset=False):
     return mesh
 
 if __name__ == '__main__':
-    '''Load mesh and print information'''
-    mesh = om.read_trimesh("assets/bunny.obj")
+    '''Apply loop subdivision over the loaded mesh using halfedge data structure'''
+    mesh = om.read_trimesh("assets/cube.obj")
+    with cProfile.Profile() as pr:
+        mesh_subdivided = subdivision_loop_halfedge(mesh, iterations=3)
+        pr.print_stats(sort='cumtime')
+    om.write_mesh("assets/assignment1/subdivision_cube_3.obj", mesh_subdivided)
     
-    # '''Apply loop subdivision over the loaded mesh'''
+    # '''Apply loop subdivision over the loaded mesh using array data structure'''
+    # mesh = trimesh.creation.box(extents=[1, 1, 1])
     # with cProfile.Profile() as pr:
-    #     mesh_subdivided = subdivision_loop(mesh, iterations=3)
+    #     mesh_subdivided = subdivision_loop_array(mesh, iterations=1)
     #     pr.print_stats(sort='cumtime')
-    # om.write_mesh("assets/assignment1/subdivision_cube_3.obj", mesh_subdivided)
+    # mesh_subdivided.export("assets/assignment1/subdivision_cube_3_array.obj")
 
     '''Apply quadratic error mesh decimation over the loaded mesh'''
+    mesh = om.read_trimesh("assets/cube.obj")
     with cProfile.Profile() as pr:
         mesh_decimated = simplify_quadric_error(mesh, face_count=2000, subset=True)
         pr.print_stats(sort='cumtime')
