e9169709ad017b950a9dc6280e12ab0532a3b46a
matudinglin <45813154+matudinglin@users.noreply.github.com>
2024-03-23 12:03:35 -0700

    Add datasets

--- code/assignment2/assignment2.py
+++ code/assignment2/assignment2.py
@@ -2,5 +2,5 @@ import subprocess
 
 if __name__ == "__main__":
     # fetch data from https://github.com/openMVG/SfM_quality_evaluation
-    subprocess.run(["python", "feat_match.py"])
-    subprocess.run(["python", "sfm.py"])
+    subprocess.run(["python", "code/assignment2/feat_match.py"])
+    subprocess.run(["python", "code/assignment2/sfm.py"])
